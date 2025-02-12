c3b0a4ecead40654afd93046a3717285099f758e
thunder04 <70254851+thunder04@users.noreply.github.com>
2025-02-12 23:32:20 +0200

    Reword titles and subtiles and use actual modified at datetime

--- src/router/opds/mod.rs
+++ src/router/opds/mod.rs
@@ -6,15 +6,15 @@ use actix_web::{HttpResponse, Responder, get, http::header, web};
 use quick_xml::se::to_string;
 use serde::Deserialize;
 use time::OffsetDateTime;
-use types::LinkType;
+use types as t;
 
-use crate::{library::Libraries, utils::determine_possesive};
+use crate::library::Libraries;
 
 pub const COMMON_ROUTE: &str = "/opds";
 
 const XMLNS_ATOM: &str = "http://www.w3.org/2005/Atom";
-const FEED_TITLE: Cow<'static, str> = Cow::Borrowed("Seshat");
-const FEED_AUTHOR: types::Author = types::Author {
+const FEED_TITLE: Cow<'static, str> = Cow::Borrowed("Seshat – OPDS Catalog");
+const FEED_AUTHOR: t::Author = t::Author {
     uri: Cow::Borrowed("https://github.com/thunder04"),
     name: Cow::Borrowed("Thunder04"),
 };
@@ -27,41 +27,39 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
 
 #[get("/")]
 async fn root(libraries: web::Data<Libraries>) -> impl Responder {
-    let entries = libraries
-        .all_libraries()
-        .map(|lib| types::Entry {
-            title: Cow::Owned(lib.name().to_string()),
-            link: types::Link {
-                href: Cow::Owned(format!("{COMMON_ROUTE}/{}/", lib.name())),
-                kind: LinkType::Acquisition.as_str(),
-                rel: None,
-            },
-
-            content: Some(types::Content {
-                kind: types::ContentKind::Text,
-                value: Cow::Owned(format!(
-                    "Explore {name}{s} catalog",
-                    s = determine_possesive(lib.name()),
-                    name = lib.name(),
-                )),
-            }),
-        })
-        .collect::<Vec<_>>();
-    let feed = types::AcquisitionFeed {
+    let feed = t::AcquisitionFeed {
         xmlns: XMLNS_ATOM,
 
         // TODO: What should I change this to? Perhaps to a hash of modified at dates of all
         // libraries?
         id: "urn:uuid:2853dacf-ed79-42f5-8e8a-a7bb3d1ae6a2".into(),
-        subtitle: Some("Explore available libraries".into()),
-        links: vec![types::Link::start()],
-        author: FEED_AUTHOR,
         title: FEED_TITLE,
+        subtitle: Some("Explore all available libraries".into()),
+        author: FEED_AUTHOR,
+        links: vec![t::Link::start()],
 
-        // TODO: Set to Modified at time of the newest metadata.db
-        updated: OffsetDateTime::now_utc(),
+        // updated_at is set as the library with the most recent change.
+        updated: libraries
+            .all_libraries()
+            .map(|lib| lib.modified_at())
+            .max()
+            .unwrap_or(OffsetDateTime::UNIX_EPOCH),
 
-        entries,
+        entries: libraries
+            .all_libraries()
+            .map(|lib| t::Entry {
+                title: lib.name().to_string().into(),
+                link: t::Link {
+                    href: format!("{COMMON_ROUTE}/{}", lib.name()).into(),
+                    kind: t::LinkType::Acquisition.as_str(),
+                    rel: None,
+                },
+                content: Some(t::Content {
+                    value: format!("Explore the \"{}\" library", lib.name()).into(),
+                    kind: t::ContentKind::Text,
+                }),
+            })
+            .collect(),
     };
 
     HttpResponse::Ok()
@@ -74,61 +72,57 @@ async fn library_root(
     lib_name: web::Path<String>, libraries: web::Data<Libraries>,
 ) -> impl Responder {
     let Some(lib) = libraries.get_library(&lib_name) else {
-        return HttpResponse::NotFound().body("the library doesn't exist");
+        return HttpResponse::NotFound().body("The library doesn't exist");
     };
 
-    let mut entries = [
-        ("By Date", "the date they were added", "date_added"),
-        ("By Title", "title", "title"),
-        ("By Author", "author", "author"),
-        ("By Language", "language", "lang"),
-        ("By Publisher", "publisher", "publisher"),
-        ("By Rating", "rating", "rating"),
-        ("By Series", "series", "series"),
-        ("By Tags", "tags", "tags"),
-    ]
-    .iter()
-    .map(|(title, sorted_by, sort)| types::Entry {
-        title: Cow::Owned(title.to_string()),
-        link: types::Link {
-            href: Cow::Owned(format!("{COMMON_ROUTE}/{lib_name}/explore?sort={sort}")),
-            kind: LinkType::Acquisition.as_str(),
-            rel: None,
-        },
-
-        content: Some(types::Content {
-            value: Cow::Owned(format!("View books sorted by {sorted_by}")),
-            kind: types::ContentKind::Text,
-        }),
-    })
-    .collect::<Vec<_>>();
-
-    entries.push(types::Entry {
+    let mut entries = vec![t::Entry {
         title: "View Books".into(),
         content: None,
-        link: types::Link {
-            href: Cow::Owned(format!("{COMMON_ROUTE}/{lib_name}/explore")),
-            kind: LinkType::Acquisition.as_str(),
+        link: t::Link {
+            href: format!("{COMMON_ROUTE}/{lib_name}/explore").into(),
+            kind: t::LinkType::Acquisition.as_str(),
             rel: None,
         },
-    });
+    }];
 
-    let subtitle = format!(
-        "Explore {lib_name}{} catalog",
-        determine_possesive(&lib_name)
+    entries.extend(
+        [
+            ("By Date", "the date they were added", "date_added"),
+            ("By Title", "title", "title"),
+            ("By Author", "author", "author"),
+            // TODO: Viewing books sorted by language? That's dumb. Group them instead.
+            ("By Language", "language", "lang"),
+            ("By Publisher", "publisher", "publisher"),
+            ("By Rating", "rating", "rating"),
+            ("By Series", "series", "series"),
+            ("By Tags", "tags", "tags"),
+        ]
+        .into_iter()
+        .map(|(title, sorted_by, sort)| t::Entry {
+            title: title.to_string().into(),
+            link: t::Link {
+                href: format!("{COMMON_ROUTE}/{lib_name}/explore?sort={sort}").into(),
+                kind: t::LinkType::Acquisition.as_str(),
+                rel: None,
+            },
+
+            content: Some(t::Content {
+                value: format!("View books sorted by {sorted_by}").into(),
+                kind: t::ContentKind::Text,
+            }),
+        }),
     );
-    let feed = types::AcquisitionFeed {
+
+    let feed = t::AcquisitionFeed {
         xmlns: XMLNS_ATOM,
 
         // TODO: What should I change this to? Perhaps to a hash of modified at date of metadata.db?
         id: "urn:uuid:2853dacf-ed79-42f5-8e8a-a7bb3d1ae6a2".into(),
-        subtitle: Some(Cow::Owned(subtitle)),
-        links: vec![types::Link::start()],
-        author: FEED_AUTHOR,
         title: FEED_TITLE,
-        // TODO: Set to Modified at time of metadata.db.
-        updated: OffsetDateTime::now_utc(),
-
+        subtitle: Some(format!("Exploring the \"{lib_name}\" library").into()),
+        author: FEED_AUTHOR,
+        updated: lib.modified_at(),
+        links: vec![t::Link::start()],
         entries,
     };
 
@@ -164,7 +158,7 @@ async fn explore_catalog(
     libraries: web::Data<Libraries>,
 ) -> impl Responder {
     let Some(lib) = libraries.get_library(&lib_name) else {
-        return HttpResponse::NotFound().body("the library doesn't exist");
+        return HttpResponse::NotFound().body("The library doesn't exist");
     };
 
     let ExploreCatalogQuery {
@@ -175,17 +169,16 @@ async fn explore_catalog(
     let limit = limit.unwrap_or(25).clamp(1, 50);
     let offset = offset.unwrap_or(0);
 
-    let feed = types::AcquisitionFeed {
+    let feed = t::AcquisitionFeed {
         xmlns: XMLNS_ATOM,
 
         // TODO: What should I change this to? Perhaps to a hash of modified at date of metadata.db?
         id: "urn:uuid:2853dacf-ed79-42f5-8e8a-a7bb3d1ae6a2".into(),
-        links: vec![types::Link::start()],
         title: FEED_TITLE,
-        author: FEED_AUTHOR,
         subtitle: None,
-        // TODO: Set to Modified at time of metadata.db.
-        updated: OffsetDateTime::now_utc(),
+        author: FEED_AUTHOR,
+        updated: lib.modified_at(),
+        links: vec![t::Link::start()],
 
         entries: vec![],
     };
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,1 @@
-pub fn determine_possesive(s: &str) -> &'static str {
-    if s.ends_with(['s', 'S']) { "'" } else { "'s" }
-}
+
